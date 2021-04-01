68dae7569af246d6124f1d349977f546c5a35705
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-01 12:00:32 +0530

    Update Bishu and his Girlfriend.cpp

--- Graph/Bishu and his Girlfriend.cpp
+++ Graph/Bishu and his Girlfriend.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(int v,int dis,vector<int> &dist,vector<int> &vis,vector<vector<int>> &ar){
+void dfs(int src, int dis, vector<int> &vis, vector<int> &dist, vector<int> al[]){
 
-    vis[v] = 1;
-    dist[v] = dis;
+    vis[src] = 1;
+    dist[src] = dis;
 
-    for(int i = 0; i < ar[v].size(); i++){
-        if(vis[ar[v][i]] == 0){
-            dfs(ar[v][i],dist[v]+1,dist,vis,ar);
+    for(int i = 0; i < al[src].size(); i++){
+
+        int child = al[src][i];
+        if(vis[child] == 0){
+            dfs(child, dist[src] + 1, vis, dist, al);
         }
     }
 }
@@ -17,39 +19,39 @@ int main(){
     int n;
     cin >> n;
 
-    vector<vector<int>> ar(n+1,vector<int>());
+    int a,b;
+    int e = n - 1;
 
-    for(int i = 1; i <= n - 1; i++){
-        int a,b;
+    vector<int> al[n+1];
+    
+    while(e--){    
         cin >> a >> b;
-        ar[a].push_back(b);
-        ar[b].push_back(a);
-    } 
+        al[a].push_back(b);
+        al[b].push_back(a);
+    }
 
     int q;
     cin >> q;
-    int city[q];
 
+    int city[q];
     for(int i = 0; i < q; i++){
         cin >> city[i];
     }
 
-    vector<int> dist(n+1,0);
+    vector<int> dist(n+1);
     vector<int> vis(n+1,0);
 
-    dfs(1,0,dist,vis,ar);
+    dfs(1,0,vis,dist,al);
 
-    int min_id = INT_MAX,min_dist = INT_MAX;
+    int min_dis = INT_MAX, min_id = INT_MAX;
     for(int i = 0; i < q; i++){
 
-        if(dist[city[i]] < min_dist){
-            min_dist = dist[city[i]];
+        if(dist[city[i]] < min_dis){
+            min_dis = dist[city[i]];
             min_id = city[i];
         }
-        else if(dist[city[i]] == min_dist){
-            if(city[i] < min_id){
-                min_id = city[i];
-            }
+        else if(dist[city[i]] == min_dis){
+            min_id = min(min_id,city[i]);
         }
     }
     cout << min_id << endl;
