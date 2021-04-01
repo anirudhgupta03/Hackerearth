1692218af639690ca944d7d24296fd05c48bec41
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-01 11:10:46 +0530

    Update Connected Components in a Graph.cpp

--- Graph/Connected Components in a Graph.cpp
+++ Graph/Connected Components in a Graph.cpp
@@ -1,38 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-void dfs(int v,vector<int> &vis,vector<vector<int>> &ar){
- 
-    vis[v] = 1;
-    for(int i = 0; i < ar[v].size(); i++){
-        if(vis[ar[v][i]] == 0){
-            dfs(ar[v][i],vis,ar);
+
+void dfs(int node, vector<int> al[], vector<int> &vis){
+
+    vis[node] = 1;
+
+    for(int i = 0; i < al[node].size(); i++){
+
+        int child = al[node][i];
+        if(vis[child] == 0){
+            dfs(child, al, vis);
         }
     }
 }
 int main(){
- 
+
     int n,e;
     cin >> n >> e;
- 
-    vector<vector<int>> ar(n+1,vector<int>());
-    
+
+    vector<int> al[n+1];
+
+    int a,b;
     while(e--){
-        int a,b;
         cin >> a >> b;
-        ar[a].push_back(b);
-        ar[b].push_back(a);
+        al[a].push_back(b);
+        al[b].push_back(a);
     }
- 
     vector<int> vis(n+1,0);
-    
     int cc_count = 0;
-    
     for(int i = 1; i <= n; i++){
+
         if(vis[i] == 0){
-            dfs(i,vis,ar);
             cc_count++;
+            dfs(i,al,vis);
         }
     }
+
     cout << cc_count << endl;
 }
