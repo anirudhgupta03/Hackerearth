f5d6b35ce8fff57987ea55738ab839d374aefaac
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-02 16:19:14 +0530

    Update Feasible relations.cpp

--- Graph/Feasible relations.cpp
+++ Graph/Feasible relations.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool dfs(int node,int cc_count,vector<int> &vis,vector<vector<int>> &ar,vector<int> &cc){
+void dfs(int node, int cc_count, vector<int> &vis, vector<int> &cc, vector<int> al[]){
 
 	vis[node] = 1;
 	cc[node] = cc_count;
 
-	for(int i = 0; i < ar[node].size(); i++){
-		if(vis[ar[node][i]] == 0){
-			dfs(ar[node][i],cc_count,vis,ar,cc);
+	for(int i = 0; i < al[node].size(); i++){
+		int child = al[node][i];
+
+		if(vis[child] == 0){
+			dfs(child, cc_count, vis, cc, al);
 		}
 	}
 }
@@ -22,24 +24,24 @@ int main(){
 		int n,k;
 		cin >> n >> k;
 
-		vector<vector<int>> ar(n+1,vector<int>());
-		vector<pair<int,int>> v;
-		
-        int a,b;
+		int a,b;
 		string s;
-		
-        while(k--){
-			cin >> a >> s >> b;
 
+		vector<int> al[n+1];
+
+		vector<pair<int,int>> v;
+
+		while(k--){
+			cin >> a >> s >> b;
+			
 			if(s == "="){
-				ar[a].push_back(b);
-				ar[b].push_back(a);
+				al[a].push_back(b);
+				al[b].push_back(a);
 			}
 			else{
 				v.push_back({a,b});
 			}
 		}
-		bool flag = true;
 
 		vector<int> cc(n+1);
 		int cc_count = 0;
@@ -49,24 +51,25 @@ int main(){
 
 			if(vis[i] == 0){
 				cc_count++;
-				dfs(i,cc_count,vis,ar,cc);
+				dfs(i, cc_count, vis, cc, al);
 			}
 		}
+		bool flag = true;
 		for(int i = 0; i < v.size(); i++){
-
+			
 			int a = v[i].first;
 			int b = v[i].second;
 
 			if(cc[a] == cc[b]){
 				flag = false;
-				break;
 			}
 		}
-		if(flag == true){
+
+		if(flag){
 			cout << "YES" << endl;
-		} 
+		}
 		else{
 			cout << "NO" << endl;
- 		}
+		}
 	}
 }
