25f92149c197e91c2197954c7caf40fb298e368f
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-21 17:29:37 +0530

    Create Kth smallest number again.cpp

---
+++ Kth smallest number again.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main(){
+
+    ll t;
+    cin >> t;
+
+    while(t--){
+
+        ll n,q;
+        cin >> n >> q;
+
+        vector<pair<ll,ll>> v;
+
+        for(int i = 0; i < n; i++){
+            ll x,y;
+            cin >> x >> y;
+            v.push_back({x,y});
+        }
+
+        sort(v.begin(),v.end());
+        ll idx = 0;
+
+        for(int i = 1; i < v.size(); i++){
+            if(v[idx].second >= v[i].first){
+                v[idx].second = max(v[idx].second,v[i].second);
+            }
+            else{
+                idx++;  
+                v[idx] = v[i];
+            }
+        }
+
+        while(q--){
+            ll k;
+            cin >> k;
+
+            ll ans = -1;
+            for(int i = 0; i <= idx; i++){
+                if((v[i].second - v[i].first + 1) >= k){
+                    ans = v[i].first + k - 1;
+                    break;
+                }
+                else{
+                    k = k - (v[i].second - v[i].first + 1);
+                }
+            }
+
+            cout << ans << endl;
+        }
+    }
+}
