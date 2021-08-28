3d48ee40e903c5271a774b4324917f689b1edea8
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-28 19:04:46 +0530

    Update Kth smallest number again.cpp

--- Kth smallest number again.cpp
+++ Kth smallest number again.cpp
@@ -52,3 +52,91 @@ int main(){
         }
     }
 }
+
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+bool cmp(pair<ll,ll> p1, pair<ll,ll> p2){
+	return p1.first < p2.first;
+}
+void merge_intervals(vector<pair<ll,ll>> &v){
+
+	sort(v.begin(),v.end(),cmp);
+
+	vector<pair<ll,ll>> res;
+
+	ll s = v[0].first, e = v[0].second;
+
+	for(int i = 1; i < v.size(); i++){
+
+		if(v[i].first <= e){
+			e = max(e, v[i].second);
+		}
+		else{
+			res.push_back({s,e});
+			s = v[i].first;
+			e = v[i].second;
+		}
+	}
+
+	res.push_back({s,e});
+
+	v.clear();
+
+	for(int i = 0; i < res.size(); i++){
+		v.push_back(res[i]);
+	}
+}
+int main(){
+
+	int t;
+	cin >> t;
+
+	while(t--){
+
+		int n,q;
+		cin >> n >> q;
+
+		vector<pair<ll,ll>> v(n);
+
+		ll a,b;
+		for(int i = 0; i < n; i++){
+			cin >> a >> b;
+			v[i] = {a,b};
+		}
+
+		merge_intervals(v);
+
+		while(q--){
+			
+			ll k;
+			cin >> k;
+
+			bool flag = false;
+
+			for(int i = 0; i < v.size(); i++){
+
+				ll a = v[i].first, b = v[i].second;
+
+				ll count = (b - a) + 1;
+
+				if(count >= k){
+					cout << a + (ll)k - 1 << endl;
+					flag = true;
+					break;
+				}
+				else{
+					k -= count;
+				}
+			}
+
+			if(!flag){
+				cout << -1 << endl;
+			}
+		}
+	}
+	return 0;
+}
